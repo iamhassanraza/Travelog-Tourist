ecb0fca679e3ea06270676d7840eb801c1b7e10f
a4m4 <salimehdi144@gmail.com>
2019-12-16 18:48:18 +0500

    Category List Done

--- CampusGruv_UI/App.js
+++ CampusGruv_UI/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import CreatePost from './Screens/CreatePost'
+
+
+
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Text> Campus Gruv </Text>
+        <CreatePost></CreatePost>
       </View>
     )
   }
