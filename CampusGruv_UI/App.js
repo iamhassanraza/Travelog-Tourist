bff81fe3276eb861546d1fa50a9c375eb54d08e3
safullah.razzaq1995@gmail.com <saifullah.razzaq1995@gmail.com>
2019-12-16 07:16:11 -0800

    'Category'

--- CampusGruv_UI/App.js
+++ CampusGruv_UI/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import { Text, View, ScrollView } from 'react-native'
+import Category from './Components/Component1';
+import {CatBrown,Catlight,CatBlue,CatGreen,Catpurple,CatLightBrown,CatLightBlue,CatSimit,CatRandom,CatService} from './Assets/Colors';
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text> Campus Gruv </Text>
-      </View>
+      <View style={{flex:1,flexDirection:'column'}}>
+        <ScrollView>
+        
+        <View style={{flexDirection:"row",marginBottom:35,marginLeft:10,marginTop:10}}>
+        <Category title="Blog " color={CatBrown} uri={require('./Assets/Images')}/>
+        <Category title="Campusgram" color={Catlight}/>
+        </View>
+      
+        <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
+        <Category title="Deals " color={Catpurple}/>
+        <Category title="DIY" color={CatBlue}/>
+        </View>
+  
+        <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
+        <Category title="Events " color={CatGreen}/>
+        <Category title="Free & For Sale" color={CatLightBrown}/>
+        </View>
+  
+        <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
+        <Category title="Housing " color={CatLightBlue}/>
+        <Category title="Jobs" color={CatLightBrown}/>
+        </View>
+
+        <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
+        <Category title="Lost & Found " color={CatLightBrown}/>
+        <Category title="News" color={CatSimit}/>
+        </View>
+
+        <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
+        <Category title="Random" color={CatRandom}/>
+        <Category title="Services" color={CatService}/>
+        </View>
+
+        </ScrollView>
+       </View>
     )
   }
 }
