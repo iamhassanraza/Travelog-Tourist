1ec6b5b9529bfe4dd5c0a3e5417e929051de0483
iamhassanraza <m.h.raxa1@gmail.com>
2019-12-17 17:44:23 +0500

    navigation and redux store added

--- CampusGruv_UI/App.js
+++ CampusGruv_UI/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import HomeScreen from './Screens/HomeScreen'
+import 'react-native-gesture-handler'
 
 import CreatePost from './Screens/CreatePost'
 
