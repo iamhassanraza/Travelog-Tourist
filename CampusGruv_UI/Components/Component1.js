f3acefc7790ec3bf0ab88edf9f21ec98bccf8d00
safullah.razzaq1995@gmail.com <saifullah.razzaq1995@gmail.com>
2019-12-18 07:42:22 -0800

    'CreatePostComplete'

--- CampusGruv_UI/Components/Component1.js
+++ CampusGruv_UI/Components/Component1.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react'
-import { Text, View, ImageBackground, TouchableOpacity } from 'react-native'
+import { Text, View, ImageBackground, TouchableOpacity, Button } from 'react-native'
 
 export default class Category extends Component {
     
-    constructor()
+    state = {
+           email:'saifullah.razzaq@yahoo.com',
+           item: [],
+           Amount:'2000',
+    }
+    itemFunc =() =>
     {
-        super();
-        this.state=
-        {
-            CategoryTitle:'',
-
-        }
+        console.log('item running')
+        this.setState((prevState) => {
+            this.state.item = prevState.item.push(this.props.amount)
+            console.log(this.state.item)
+        })   
     }
     render() {
-        console.log(this.state.CategoryTitle)
+console.log('rendeering');
 
         return (
-            <View style={{flex:1,height:110,opacity:0.7}}>
-     <TouchableOpacity onPress={()=>{this.setState({CategoryTitle:this.props.title})}}>
-            <ImageBackground source={require('../Assets/Images/book.jpg')} style={{width:160,height:120,borderRadius:5}}>
-     <View style={{width:'100%',height:'100%',backgroundColor:this.props.color}}>
-     <Text style={{alignSelf:'center',color:'#fff',fontSize:15,fontWeight:"bold"}}>{this.props.title}</Text>
+            <View style={{flex:1,height:100}}>
+     <TouchableOpacity onPress={this.itemFunc}>
+            <ImageBackground source={{uri:this.props.uri}} style={{width:160,height:120,borderRadius:5}}>
+     <View style={{backgroundColor:this.props.color,flexDirection:'row',justifyContent:'space-between'}}>
+     <Text style={{alignSelf:'flex-start',color:'white',fontSize:17,fontWeight:"bold"}}>{this.props.title}</Text>
+    <View>
+     <Text style={{alignSelf:'flex-end',color:'white',fontSize:15,fontWeight:"bold"}}>{this.props.amount}</Text>
+    </View>
 
      </View>
             </ImageBackground>
      </TouchableOpacity>
- </View>
+    
+
+
+    </View>
  
  
         )
--- CampusGruv_UI/Screens/CategoryList.js
+++ CampusGruv_UI/Screens/CategoryList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View, ScrollView } from 'react-native'
+import {Header} from 'react-native-elements'
 import Category from '../Components/Component1';
 import {CatBrown,Catlight,CatBlue,CatGreen,Catpurple,CatLightBrown,CatLightBlue,CatSimit,CatRandom,CatService} from '../Assets/Colors';
 export default class CategoryList extends Component {
@@ -7,36 +8,36 @@ export default class CategoryList extends Component {
     return (
       <View style={{flex:1,flexDirection:'column'}}>
         <ScrollView>
-        
+        <Header containerStyle={{width:'100%',height:60,backgroundColor:CatLightBlue}}
+  leftComponent={{ icon: 'menu', color: '#fff' }}
+  centerComponent={{ text: 'Dashboard', style: { color: '#fff',fontWeight:'bold' } }}
+  rightComponent={{ icon: 'home', color: '#fff' }}
+/>
         <View style={{flexDirection:"row",marginBottom:35,marginLeft:10,marginTop:10}}>
-        <Category title="Blog " color={CatBrown}/>
-        <Category title="Campusgram" color={Catlight}/>
+        <Category title="Borritos " color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/Burrito-1.jpg' amount='500'/>
+        <Category title="Bowls" color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/Bowl.jpg' amount='500'/>
         </View>
       
         <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
-        <Category title="Deals " color={Catpurple}/>
-        <Category title="DIY" color={CatBlue}/>
+        <Category title="Arepas " color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/Quesidilla.jpg' amount='500'/>
+        <Category title="Empanadas" color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/Arepa.jpg' amount='500'/>
         </View>
   
         <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
-        <Category title="Events " color={CatGreen}/>
-        <Category title="Free & For Sale" color={CatLightBrown}/>
+        <Category title="Tacos " color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/Tacos.jpg' amount='500'/>
+        <Category title="Desert" color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/Sides.jpg' amount='500'/>
         </View>
   
         <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
-        <Category title="Housing " color={CatLightBlue}/>
-        <Category title="Jobs" color={CatLightBrown}/>
+        <Category title="Kit Bowl " color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/07/Bowl_Chips_Web-1.jpg' amount='500'/>
+        <Category title="Kid Borritos" color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/KIDS-BURRITOS.jpg' amount='500'/>
         </View>
 
         <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
-        <Category title="Lost & Found " color={CatLightBrown}/>
-        <Category title="News" color={CatSimit}/>
+        <Category title="Beverages " color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/BEVERAGES.jpg' amount='500'/>
+        <Category title="Cold" color={CatBlue} uri='https://www.andalelatinogrill.com/wp-content/uploads/2017/06/BEVERAGES.jpg'amount='500' />
         </View>
 
-        <View style={{flexDirection:"row",marginBottom:35,marginLeft:10}}>
-        <Category title="Random" color={CatRandom}/>
-        <Category title="Services" color={CatService}/>
-        </View>
 
         </ScrollView>
        </View>
