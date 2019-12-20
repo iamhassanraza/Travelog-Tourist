8fc9f6af8e02e78c26b3d4d0a7aadbcf68ce33ed
saifullahrazzaq1995@gmail.com <saifullah.razzaq1995@gmail.com>
2019-12-20 03:34:07 -0800

    workdone

--- CampusGruv_UI/Components/CategoryComp.js
+++ CampusGruv_UI/Components/CategoryComp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View, ImageBackground, TouchableOpacity, Button } from 'react-native'
+
 export default class Category extends Component {
     
     state = {
@@ -8,17 +9,20 @@ export default class Category extends Component {
    
     render() {
         return (
-            <View style={{flex:1,height:100}}>
+            <View style={{flex:1,height:100,justifyContent:'center',alignItems:'center'}}>
      <TouchableOpacity>
-            <ImageBackground source={{uri:this.props.uri}} style={{width:160,height:120,borderRadius:5,opacity:0.6}}>
+            <ImageBackground source={{uri:this.props.uri}} style={{width:160,height:105,borderRadius:5,opacity:0.6}}>
      <View style={{width:'100%',height:'100%',backgroundColor:this.props.color,}}>
       <View>
-     <Text style={{alignSelf:'center'}}>{this.props.title}</Text>
+     <Text style={{alignSelf:'center',color:'white',fontSize:12,fontWeight:'bold'}}>{this.props.title}</Text>
     </View>
+
      </View>
             </ImageBackground>
      </TouchableOpacity>
     
+
+
     </View>
  
  
