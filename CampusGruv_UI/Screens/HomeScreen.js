b01152d6e22b5408b24c254c72f5e3f679b1eed8
iamhassanraza <m.h.raxa1@gmail.com>
2019-12-26 20:12:51 +0500

    asdas

--- CampusGruv_UI/Screens/HomeScreen.js
+++ CampusGruv_UI/Screens/HomeScreen.js
@@ -1,44 +1,24 @@
-import React, {Component} from 'react';
-import {
-  Text,
-  View,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet,
-  FlatList,
-} from 'react-native';
-import PostsList from '../Components/PostsList';
-import Masonry from 'react-native-masonry-layout';
+import React, { Component } from 'react'
+import { Text, View, ScrollView, SafeAreaView, StyleSheet, FlatList } from 'react-native'
+import PostsList from '../Components/PostsList'
 
 export default class HomeScreen extends Component {
 
-    const data = this.refs.masonry.addItems([
-        { key:1, text:"text1" },
-        { key:1, text:"text1" }
-    ]);
 
+    render() {
 
-
-  render() {
-    return (
-      <View style={styles.container}>
-        {/* <PostsList />  */}
-        <Masonry
-          ref="masonry"
-          columns={3} // optional - Default: 2
-          renderItem={data => (
-            <View>
-              <Text>some text</Text>
+        return (
+            <View 
+                style={styles.container}
+            >
+                <PostsList /> 
             </View>
-          )}
-        />
-      </View>
-    );
-  }
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: '99%',
-  },
-});
+    container: {
+        height:'99%'
+    }
+})
