bd83a67645862dac34ec550b339854dad01976bc
iamhassanraza <m.h.raxa1@gmail.com>
2020-01-08 17:00:06 +0500

    hassan

--- CampusGruv_UI/Screens/UserProfile.js
+++ CampusGruv_UI/Screens/UserProfile.js
@@ -14,11 +14,21 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import RenderCards from '../Components/RenderCards';
-import NoPosts from '../Components/NoPost'
+import NoPosts from '../Components/NoPost';
 import ContentLoader, {Rect} from 'react-content-loader/native';
-import { connect } from "react-redux";
-import { CreateUserDetails } from "../ReduxStore/Actions/index";
-
+import {connect} from 'react-redux';
+import {CreateUserDetails} from '../ReduxStore/Actions/index';
+import {
+  BallIndicator,
+  BarIndicator,
+  DotIndicator,
+  MaterialIndicator,
+  PacmanIndicator,
+  PulseIndicator,
+  SkypeIndicator,
+  UIActivityIndicator,
+  WaveIndicator,
+} from 'react-native-indicators';
 
 class UserProfile extends React.Component {
   static navigationOptions = {
@@ -29,6 +39,8 @@ class UserProfile extends React.Component {
     this.state = {
       posts: [],
       total: undefined,
+      active: 'posts',
+      spinner:false
     };
   }
 
@@ -47,22 +59,43 @@ class UserProfile extends React.Component {
 
   fetchdata = async () => {
     console.log('calling');
-    const userId = await AsyncStorage.getItem('USER_ID');
-    const Token = await AsyncStorage.getItem('TOKEN');
-    const response = await fetch(
-      `https://campus-gruv-heroku.herokuapp.com/api/v1/search/user?type=post&user_id=${userId}&page=1`,
-      {
-        headers: {
-          Authorization: `Bearer ${Token}`,
+    this.setState({spinner:true})
+    if (this.state.active === 'posts') {
+      const userId = await AsyncStorage.getItem('USER_ID');
+      const Token = await AsyncStorage.getItem('TOKEN');
+      const response = await fetch(
+        `https://campus-gruv-heroku.herokuapp.com/api/v1/search/user?type=post&user_id=${userId}&page=1`,
+        {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
         },
-      },
-    );
-    const jsonresponse = await response.json();
-    console.log(jsonresponse)
-    this.setState({
-      posts: jsonresponse.data,
-      total: jsonresponse.total,
-    });
+      );
+      const jsonresponse = await response.json();
+      console.log(jsonresponse);
+      this.setState({
+        spinner:false,
+        posts: jsonresponse.data,
+        total: jsonresponse.total,
+      });
+    } else {
+      const Token = await AsyncStorage.getItem('TOKEN');
+      const response = await fetch(
+        `https://campus-gruv-heroku.herokuapp.com/api/v1/fetch/saved/posts`,
+        {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
+        },
+      );
+      const jsonresponse = await response.json();
+      console.log(jsonresponse);
+      this.setState({
+        spinner:false,
+        posts: jsonresponse.data,
+        total: jsonresponse.total,
+      });
+    }
   };
 
   _signOutAsync = async () => {
@@ -70,46 +103,42 @@ class UserProfile extends React.Component {
     this.props.navigation.navigate('Auth');
   };
 
-
-renderPost = () =>{
-  console.log("renderpost");
-  return(
-    <View style={{paddingTop: 10}}>
-          <RenderCards posts={this.state.posts}></RenderCards>
-          </View>
-  )
-}
-
-renderNoPost = () => {
-  console.log("nopost");
-  return(
-<View style={{paddingTop:30, height:"100%"}}>
-      <NoPosts></NoPosts>
+  renderPost = () => {
+    console.log('renderpost');
+    return (
+      <View style={{paddingTop: 10}}>
+        <RenderCards posts={this.state.posts}></RenderCards>
       </View>
-  )
-}
+    );
+  };
 
+  renderNoPost = () => {
+    console.log('nopost');
+    return (
+      <View style={{paddingTop: 30, height: '100%'}}>
+        <NoPosts></NoPosts>
+      </View>
+    );
+  };
 
-
-renderLoading = () => {
-  return(
-    <View>
-    <ContentLoader
-      height={450}
-      width={820}
-      speed={0.2}
-      height={Dimensions.get('window').height * 1}>
-      <Rect x="10" y="10" rx="5" ry="5" width="185" height="220" />
-      <Rect x="200" y="10" rx="5" ry="5" width="200" height="280" />
-      <Rect x="10" y="240" rx="5" ry="5" width="185" height="250" />
-      <Rect x="200" y="300" rx="5" ry="5" width="200" height="280" />
-      {/* <Rect x="280" y="300" rx="5" ry="5" width="260" height="140" />
+  renderLoading = () => {
+    return (
+      <View>
+        <ContentLoader
+          height={450}
+          width={820}
+          speed={0.2}
+          height={Dimensions.get('window').height * 1}>
+          <Rect x="10" y="10" rx="5" ry="5" width="185" height="220" />
+          <Rect x="200" y="10" rx="5" ry="5" width="200" height="280" />
+          <Rect x="10" y="240" rx="5" ry="5" width="185" height="250" />
+          <Rect x="200" y="300" rx="5" ry="5" width="200" height="280" />
+          {/* <Rect x="280" y="300" rx="5" ry="5" width="260" height="140" />
               <Rect x="550" y="160" rx="5" ry="5" width="260" height="280" /> */}
-    </ContentLoader>
-  </View>
-  )
-}
-
+        </ContentLoader>
+      </View>
+    );
+  };
 
   render() {
     // const { navigate } = this.props.navigation;
@@ -160,19 +189,21 @@ renderLoading = () => {
         </View>
 
         {/* IMAGE and NAME  */}
-        <View style={{flexDirection: 'row', marginLeft: 5, alignItems:"center"}}>
+        <View
+          style={{flexDirection: 'row', marginLeft: 5, alignItems: 'center'}}>
           <Image
             source={{
-              uri: this.props.User.profile_pic_url
+              uri: this.props.User.profile_pic_url,
             }}
             style={{width: 80, height: 80, borderRadius: 50}}
           />
-          <View style={{ marginLeft: 5}}>
+          <View style={{marginLeft: 5}}>
             <Text style={{fontSize: 25, fontWeight: 'bold', color: '#727272'}}>
-            {this.props.User.first_name+' '+ this.props.User.last_name} 
+              {this.props.User.first_name + ' ' + this.props.User.last_name}
             </Text>
             <Text style={{fontSize: 13, color: '#727272'}}>
-              University of Pittsurgh
+             {/* {this.props.User.campuses.description} */}
+             {console.log(this.props.User)}
             </Text>
           </View>
         </View>
@@ -205,7 +236,7 @@ renderLoading = () => {
             flexDirection: 'row',
             marginTop: 10,
             justifyContent: 'space-around',
-            alignItems:"center"
+            alignItems: 'center',
           }}>
           <View
             style={{
@@ -215,26 +246,46 @@ renderLoading = () => {
               width: '65%',
               marginLeft: 0,
               borderRadius: 12,
-              paddingTop:"2%",
-              paddingLeft:"2%",
-              height:"75%"
+              paddingTop: '2%',
+              paddingLeft: '2%',
+              height: '75%',
             }}>
-            <Icon
-              name="search"
-              color="#C4C4C4"
-              size={20}
-              style={{}}
-            />
+            <Icon name="search" color="#C4C4C4" size={20} style={{}} />
             <TextInput
-              style={{width: '100%', fontSize: 15, color: '#ACACAC', paddingTop:0}}
+              style={{
+                width: '100%',
+                fontSize: 15,
+                color: '#ACACAC',
+                paddingTop: 0,
+              }}
               placeholder="Search"
               // value={this.state.password}
               // onChangeText={password => this.setState({ password })}
             />
           </View>
 
-          <View style={{flexDirection: 'row', marginTop: 0, marginLeft: 0, alignItems:"center", height:"70%",paddingRight:"3%",paddingLeft:"1%"}}>
-            <Text style={{fontSize: 17, fontWeight: 'bold', color: '#0C91CF'}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginTop: 0,
+              marginLeft: 0,
+              alignItems: 'center',
+              height: '70%',
+              paddingRight: '3%',
+              paddingLeft: '1%',
+            }}>
+            <Text
+              onPress={() => {
+                this.setState({active: 'posts'},()=>{
+                  this.fetchdata()
+                });
+                
+              }}
+              style={{
+                fontSize: 17,
+                fontWeight: 'bold',
+                color: this.state.active === 'posts' ? '#0C91CF' : '#B4B8BA',
+              }}>
               Posts{' '}
             </Text>
             <View
@@ -246,36 +297,41 @@ renderLoading = () => {
                 backgroundColor: '#B4B8BA',
               }}
             />
-            <Text style={{fontSize: 17, fontWeight: 'bold', color: '#B4B8BA'}}>
+            <Text
+              onPress={() => {
+                this.setState({active: 'saves'},()=>{
+                  this.fetchdata()
+                });
+              }}
+              style={{
+                fontSize: 17,
+                fontWeight: 'bold',
+                color: this.state.active === 'saves' ? '#0C91CF' : '#B4B8BA',
+              }}>
               {' '}
               Saves
             </Text>
           </View>
         </View>
 
-{/* {this.state.total === 0 ? <View style={{height:"70%"}}><NoPosts></NoPosts></View> : <Text>Loader</Text>}
+        {/* {this.state.total === 0 ? <View style={{height:"70%"}}><NoPosts></NoPosts></View> : <Text>Loader</Text>}
 {this.state.total > 0 ? <View style={{paddingTop: 10}}>
           <RenderCards posts={this.state.posts}></RenderCards>
         </View> : <Text>Loader>>></Text>} */}
- 
-{this.state.total === 0 ? this.renderNoPost() : (this.state.total > 0 ? this.renderPost() : this.renderLoading())}
-      
-<View style={{paddingTop: 10}}>
-          <RenderCards posts={this.state.posts}></RenderCards>
-          </View>
+           {this.state.spinner ? this.renderLoading() : (this.state.total === 0 ? this.renderNoPost() : this.renderPost())}
+
+
       </ScrollView>
     );
   }
 }
 
-
-mapStateToProps = (state)=>{ //this state will contain FULL redux store all the reducers data
-
+mapStateToProps = state => {
+  //this state will contain FULL redux store all the reducers data
 
   //use your required reducer data in props i.e reducer1
-  
-  return { User : state.User}  //isse ye reducer1 wala data as a props ajaega is component me (combinereducer me jo key assign ki thi wo use karna)
-  
-  }
 
-export default connect(mapStateToProps,null)(UserProfile); 
+  return {User: state.User}; //isse ye reducer1 wala data as a props ajaega is component me (combinereducer me jo key assign ki thi wo use karna)
+};
+
+export default connect(mapStateToProps, null)(UserProfile);
