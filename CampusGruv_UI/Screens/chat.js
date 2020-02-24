669a058e5e0ef41654dcd2e68bbd28d109b98eb5
unknown <faraznaqvi99@gmail.com>
2020-02-24 17:27:30 +0500

    chat soon..

--- CampusGruv_UI/Screens/chat.js
+++ CampusGruv_UI/Screens/chat.js
@@ -35,10 +35,56 @@ class Chat extends React.Component {
     }
   }
 
-  state = {
-    messages: [],
-    connected: false,
-    room_id: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      messages2:
+        [
+          {
+            _id: 484,
+            text: 'Hello developer',
+            //createdAt: new Date(),
+            user: {
+              _id: 486,
+              name: 'React Native',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          },
+          {
+            _id: 6,
+            text: 'second in array',
+            //createdAt: new Date(),
+            user: {
+              _id: 484,
+              name: this.props.User.first_name + ' ' + this.props.User.last_name,
+              avatar: this.props.User.profile_pic_url,
+            },
+          },
+          {
+            _id: 2,
+            text: 'Hello Nibba',
+            //createdAt: new Date(),
+            user: {
+              _id: 486,
+              name: 'React Native',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          },
+          {
+            _id: 7,
+            text: 'Hello Nibba',
+            //createdAt: new Date(),
+            user: {
+              _id: 484,
+              name: 'React Native',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          }
+      ],
+      connected: false,
+      room_id: null,
+    };
   }
 
   fetchRoomDetails = async () => {
@@ -57,18 +103,17 @@ class Chat extends React.Component {
     })
   }
 
-  mapMessages = (msgs) => {
-    this.setState({
-      messages: msgs.map(m => {
-        return {
-          text: m.message,
-          user: {
-            _id: m.user_id,
-            name: m.name,
-            avatar: m.profile_pic_url
-          }
+  mapMessages = (msgs) => { 
+    return msgs.map(m => {
+      return {
+        //_id: m.user_id === this.props.User.id ? 486 : 484,
+        text: m.message,
+        user: {
+          _id: m.user_id,
+          name: m.name,
+          avatar: m.profile_pic_url
         }
-      })
+      }
     })
   }
 
@@ -81,9 +126,14 @@ class Chat extends React.Component {
       this.socket.emit('joinRoom')
     });
     this.socket.on('joinRoom', (msgs) => {
-      console.log('messages ========> ',msgs)
-      this.mapMessages(msgs)
-    });
+      //console.log('messages ========> ',msgs)
+      const tempArray = this.mapMessages(msgs)
+      console.log("temporary arrrayyy ",tempArray)
+      this.setState( previousState => ({
+        messages: GiftedChat.append([previousState.messages, tempArray])
+      }) 
+      );
+    })
     this.socket.on('error', () => {
       console.log('hello jee error established')
     });
@@ -96,22 +146,36 @@ class Chat extends React.Component {
   }
 
 
-  onSend(messages) {
-    this.setState(previousState => {
-      tempArray = []
-      previousState.messages.unshift(messages)
-      console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^",previousState.messages)
-      tempArray = this.mapMessages(previousState.messages)
-      console.log("*********************************************************",tempArray)
-      return {
-      messages: tempArray,
-    }
-  })
+
+  onSend = async (messages = []) => {
+    //this.setState(previousState => {
+      // var tempArray = []
+      // // tempArray = previousState.messages.reverse()
+      // // console.log('reversed array is >>>>>>>>>>>>>>.',tempArray)
+      // console.log('message ============ >',messages)
+      // console.log('previous state messages =====>',previousState.messages)
+      // tempArray = GiftedChat.append( messages, previousState.messages)
+      // console.log('temp array is >>>>>>>>>>>>>>.',tempArray)
+      // // var tempArray = []
+      // // previousState.messages.unshift(messages[0])
+      // // console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^",previousState.messages)
+      // // tempArray = this.mapMessages(previousState.messages)
+      // // console.log("*********************************************************",tempArray)
+      // return {
+      //   messages: tempArray
+      // }
+    // })
+    console.log('original msg is *****', messages)
+    console.log('previous messages are *****', this.state.messages)
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
   }
 
-  //GiftedChat.append(previousState.messages, messages)
+  // GiftedChat.append(previousState.messages, messages)
 
   render() {
+    console.log('this.state.messages =========> ',this.state.messages)
     return (
         <GiftedChat
         messages={this.state.messages}
@@ -119,7 +183,7 @@ class Chat extends React.Component {
         onSend={messages => this.onSend(messages)}
         user={{
           _id: this.props.User.id,
-          name: this.props.User.name,
+          name: this.props.User.first_name + ' ' + this.props.User.last_name,
           avatar: this.props.User.profile_pic_url
         }}
       />
