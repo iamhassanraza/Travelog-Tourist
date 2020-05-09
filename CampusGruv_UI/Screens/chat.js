fcc072fd37837c6d597c5aa96a2b6f6215ce5e16
mac <mac@macs-iMac.local>
2020-05-09 22:30:33 +0500

    mehnat mehnat mehnat

--- CampusGruv_UI/Screens/chat.js
+++ CampusGruv_UI/Screens/chat.js
@@ -5,13 +5,19 @@ import {
   Text,
   TouchableOpacity,
   AppState,
+  Dimensions,
   AsyncStorage,
+  Image,
 } from 'react-native';
+import {Icon, Input, Item} from 'native-base';
 import PostIcon from 'react-native-vector-icons/MaterialIcons';
 //import AndroidKeyboardAdjust from 'react-native-android-keyboard-adjust';
 import io from 'socket.io-client';
 import {connect} from 'react-redux';
 import {CreateUserDetails} from '../ReduxStore/Actions/index';
+import {ThemeBlue} from '../Assets/Colors';
+import TimeAgo from 'react-native-timeago';
+import ImagePicker from 'react-native-image-picker';
 
 class Chat extends React.Component {
   static navigationOptions = props => {
@@ -56,15 +62,168 @@ class Chat extends React.Component {
     this.state = {
       messages: [],
       connected: false,
+      currentMessage: '',
       room_id: null,
     };
   }
+
+  customComposer = props => {
+    <View
+      style={{
+        marginBottom:
+          Platform.OS == 'ios'
+            ? Dimensions.get('window').height > 800
+              ? 20
+              : 10
+            : 10,
+        height: 50,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderTopColor: 'grey',
+        paddingTop: 5,
+        alignItems: 'center',
+      }}>
+      <View style={{marginLeft: '2%', width: 30, height: 30}}>
+        <Icon type="Feather" name="camera" style={{fontSize: 25}} />
+      </View>
+      <Item
+        style={{
+          marginLeft: 5,
+          borderBottomWidth: 0,
+          width: Dimensions.get('window').width - 100,
+        }}>
+        <Input
+          value={this.state.currentMessage}
+          getRef={input => {
+            this.commentInput = input;
+          }}
+          multiline={true}
+          placeholder="Add a comment"
+          style={{
+            borderWidth: 0.3,
+            padding: 2,
+            borderRadius: 10,
+            borderColor: 'grey',
+          }}
+          onChangeText={text => {
+            this.setState({
+              currentComment: text,
+            });
+          }}
+        />
+      </Item>
+      <View
+        style={{
+          width: 60,
+          height: 40,
+          paddingRight: '2%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            //CALL API FOR COMMENT , USER ID ,POST ID , COMMENT DESCRIPTION
+            this.postComment(postId, userId);
+          }}>
+          <Text
+            style={{
+              fontSize: 18,
+              color:
+                this.state.currentComment === null ||
+                this.state.currentComment === ''
+                  ? '#e8d7d5'
+                  : ThemeBlue,
+              //paddingLeft: '2%',
+            }}>
+            Post
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>;
+  };
+
+  customMessage = props => {
+    // console.log('message props', props);
+    return (
+      <View
+        style={{
+          marginHorizontal: '2%',
+          marginVertical: '1%',
+          alignSelf:
+            props.currentMessage.user._id === this.props.User.id
+              ? 'flex-end'
+              : 'flex-start',
+        }}>
+        <View
+          style={{
+            width: '100%',
+            //alignItems: 'center',
+            flexDirection: 'row',
+          }}>
+          <Image
+            source={{uri: props.currentMessage.user.avatar}}
+            style={{
+              width: 30,
+              marginBottom: 8,
+              height: 30,
+              borderWidth: 1,
+              alignSelf: 'flex-end',
+              borderRadius: 50,
+            }}
+          />
+          <View style={{marginLeft: 5}}>
+            <Text style={{fontSize: 10, paddingLeft: '1%'}}>
+              {props.currentMessage.user.name.split(' ')[0] +
+                ' ' +
+                props.currentMessage.user.name.split(' ')[1].charAt('0') +
+                '.'}
+            </Text>
+            <View
+              style={{
+                marginTop: 2,
+                justifyContent: 'center',
+                maxWidth: 250,
+                paddingVertical: '1%',
+              }}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  borderRadius: 8,
+                  borderColor: ThemeBlue,
+                  borderWidth: 1,
+                  fontSize: 14,
+                  paddingVertical: 2,
+                  paddingHorizontal: '2%',
+                }}>
+                {props.currentMessage.text}
+              </Text>
+            </View>
+            {this.state.imageSource ? (
+              <Image
+                source={this.state.imageSource}
+                style={{width: 200, marginTop: 10, height: 150}}
+              />
+            ) : null}
+            <Text style={{marginLeft: '1%', marginTop: 2, color: 'grey'}}>
+              <TimeAgo
+                time={
+                  new Date(props.currentMessage.created_at.replace(' ', 'T'))
+                }
+                style={{fontSize: 8}}
+              />
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
 
   mapMessages = msgs => {
     return msgs.map(m => {
       return {
         _id: m.id,
         text: m.message,
+        created_at: m.created_at,
         user: {
           _id: m.user_id,
           name: m.name,
@@ -74,21 +233,13 @@ class Chat extends React.Component {
     });
   };
 
-  _handleAppStateChange = nextAppState => {
-    console.log(nextAppState, 'haha');
-    // if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
-    //   console.log('App has come to the foreground!')
-    // }
-    //this.setState({appState: nextAppState});
-  };
-
   async componentDidMount() {
     AppState.addEventListener('change', this._handleAppStateChange);
     this.props.socket.emit('joinRoom', {
       room_id: `${this.props.navigation.getParam('room_id', null)}`,
     });
     this.props.socket.on('joinRoom', async msgs => {
-      console.log('room joined', msgs);
+      // console.log('room joined', msgs);
       if (msgs !== null) {
         var tempArray = await this.mapMessages(msgs);
       } else {
@@ -126,8 +277,10 @@ class Chat extends React.Component {
   }
 
   onSend = async (messages = []) => {
+    console.log('sent', messages);
     const tempArray = messages.map(m => {
       return {
+        created_at: m.createdAt,
         user_id: m.user._id,
         message: m.text,
         profile_pic_url: m.user.avatar,
@@ -138,15 +291,91 @@ class Chat extends React.Component {
     this.props.socket.emit('message', tempArray[0]);
   };
 
+  selectPhoto = () => {
+    ImagePicker.showImagePicker(
+      {
+        maxWidth: 1000,
+        maxHeight: 1000,
+        storageOptions: {
+          skipBackup: true,
+          path: 'images',
+          cameraRoll: true,
+          waitUntilSaved: true,
+        },
+      },
+      response => {
+        if (response.didCancel) {
+        } else if (response.error) {
+        } else {
+          const source = {uri: response.uri};
+          const fileTypes = /jpeg|jpg|png|gif/;
+          const allowedImgSize = 1024 * 1024 * 10;
+          // console.log('response image: ', response);
+          if (!fileTypes.test(response.type)) {
+            alert(
+              'Uploaded file is not a valid image. \n(allowed file types: jpeg, jpg, png, gif)',
+            );
+          } else if (response.fileSize > allowedImgSize) {
+            alert('Uploaded file is too large \n(allowed file size is 10MB)');
+          } else {
+            this.state.imgCount++;
+            this.setState({
+              Images: response,
+              imageSource: source,
+            });
+            //setImagess(Imagess.concat(response.fileName));
+          }
+        }
+      },
+    );
+  };
+
   render() {
     return (
       <GiftedChat
+        listViewProps={{
+          style: {
+            backgroundColor: '#f9fdfe',
+          },
+        }}
+        compos
+        alwaysShowSend={true}
+        renderMessage={props => this.customMessage(props)}
+        textInputProps={{
+          borderRadius: 10,
+          placeholder: 'message...',
+          borderWidth: 1,
+          borderColor: 'grey',
+          marginTop: 10,
+          justifyContent: 'center',
+          paddingLeft: '2%',
+          paddingRight: '2%',
+          fontSize: 16,
+          backgroundColor: 'white',
+        }}
+        renderActions={() => (
+          <Icon
+            onPress={() => this.selectPhoto()}
+            type="Feather"
+            name="camera"
+            style={{
+              color: 'grey',
+              marginLeft: '3%',
+              marginBottom: 2,
+              fontSize: 32,
+            }}
+          />
+        )}
         messages={this.state.messages}
         showUserAvatar={true}
         onSend={messages => this.onSend(messages)}
         user={{
           _id: this.props.User.id,
-          name: this.props.User.first_name + ' ' + this.props.User.last_name,
+          name:
+            this.props.User.first_name +
+            ' ' +
+            this.props.User.last_name.charAt('0') +
+            '.',
           avatar: this.props.User.profile_pic_url,
         }}
       />
